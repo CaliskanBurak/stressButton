7773e7df25dd00391923c80b9466197160203ac9
Wilmer <wilmersinchi@gmail.com>
2020-07-22 22:58:20 -0400

    done

--- stressButton/popup.js
+++ stressButton/popup.js
@@ -4,8 +4,26 @@ document.addEventListener(
     document.querySelector("button").addEventListener("click", onclick, false);
 
     function onclick() {
+      let urlArray = [
+        "https://www.youtube.com/watch?v=p5C41AFtAgA",
+        "https://www.youtube.com/watch?v=cKqZ5mvHfF0",
+        "https://www.youtube.com/watch?v=PgD56JEUWFA",
+        "https://www.youtube.com/watch?v=tL8_I7S9siU",
+        "https://www.youtube.com/watch?v=SB-qEYVdvXA",
+        "https://www.youtube.com/watch?v=cgib0bCzUpg",
+      ];
+      let randomNumber = Math.floor(Math.random() * 5);
+
+      let previous;
+      previous = Math.floor(Math.random() * 5);
+      if (previous === randomNumber) {
+        previous = Math.floor(Math.random() * 5);
+      }
+
       chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, "hi");
+        chrome.tabs.create({ url: urlArray[previous] }, function (tab) {
+          chrome.tabs.executeScript(tab.id, { file: "popup.js" });
+        });
       });
     }
   },
